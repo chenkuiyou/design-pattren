c248e765f784f4de5c821da9c7a4ea317eb0146f
cky <2569421710@qq.com>
2020-02-28 18:39:39 +0800

    备忘录模式（就写了概念）

---
+++ memento/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * 备忘录模式， 类似于日志，用于回滚
+ * 比如 git的本地仓库，可以进行版本回滚
+ * 一般对象都要实现 Seriazable ； 存到磁盘中，需要的时候要读回来，
+ * 在写的时候要注意，什么属性先写，读的时候什么先读
+ * @author cky
+ * @create 2020 02 28 14:54
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
