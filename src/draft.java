081c368a5702fbb5851cbd1add8cabfc95760024
cky <2569421710@qq.com>
2020-02-28 20:03:33 +0800

    提交整个工程

--- src/draft.java
+++ src/draft.java
@@ -7,37 +7,8 @@ import java.util.List;
  */
 public class draft {
     public static void main(String[] args) {
-        List l=new ArrayList();
-        List l2=new ArrayList();
-        l.add(1);
-        l.add(1);
-        l.add(3);
-        l2.add(4);
-        l2.add(6);
-        l2.add(5);
-        l.add(l2);
-        for (Object i:l)
-            System.out.println(i);
-    }
-}
+        int []a={2,0,1};
+        //System.out.println(5 / 2);
 
-class Solution {
-    public boolean findNumberIn2DArray(int[][] matrix, int target) {
-        if(matrix==null)
-            return false;
-        if(matrix.length==0||matrix[0].length==0)
-            return false;
-        int n=matrix.length;
-        int m=matrix[0].length;
-        int i=0;
-        int j=m-1;
-        while(i<n&&j>-1){
-            if(target>matrix[i][j])
-                i++;
-            else if(target<matrix[i][j])
-                j--;
-            else return true;
-        }
-        return false;
     }
 }
