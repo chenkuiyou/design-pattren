6c7185a7caa738165dc32cc74440dedcc39cfaa3
cky <2569421710@qq.com>
2020-03-04 14:23:47 +0800

    修改了原型模式的介绍

--- src/draft.java
+++ src/draft.java
@@ -1,5 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.tree.TreeNode;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * @author cky
@@ -7,8 +8,62 @@ import java.util.List;
  */
 public class draft {
     public static void main(String[] args) {
-        int []a={2,0,1};
-        //System.out.println(5 / 2);
+        Queue<String> queue = new LinkedList<String>();
 
+        Solution s=new Solution();
+
+        //System.out.println(s.validateStackSequences(a, b));
+    }
+}
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root==null)
+            return new ArrayList<List<Integer>>();
+        TreeNode last=root;
+        TreeNode lastf=root;
+        boolean trun=true;
+        List<TreeNode>  l1=new ArrayList<>();
+        List<TreeNode>  l2=new ArrayList<>();
+        l1.add(root);
+        List<List<Integer>>  res=new ArrayList<List<Integer>>();
+        res.add(new ArrayList<>());
+        res.get(0).add(root.val);
+        res.add(new ArrayList<>());
+        while(l1.size()!=0||l2.size()!=0){
+            while(l1.size()>0){
+                TreeNode t=l1.get((l1.size()-1));
+                l1.remove(l1.size()-1);
+                if(t.right!=null){
+                    l2.add(t.right);
+                    res.get(res.size()-1).add(t.right.val);
+                }
+                if(t.left!=null){
+                    l2.add(t.left);
+                    res.get(res.size()-1).add(t.left.val);
+                }
+            }
+            if(l2.size()==0)
+                break;
+            res.add(new ArrayList<>());
+            while(l2.size()>0){
+                TreeNode t=l2.get((l2.size()-1));
+                l2.remove(l2.size()-1);
+                if(t.left!=null){
+                    l1.add(t.left);
+                    res.get(res.size()-1).add(t.left.val);
+
+                }
+                if(t.right!=null){
+                    l1.add(t.right);
+                    res.get(res.size()-1).add(t.right.val);
+                }
+            }
+            res.add(new ArrayList<>());
+        }
+        while(res.get(res.size()-1).size()==0){
+            res.remove(res.size()-1);
+        }
+        return res;
     }
 }
